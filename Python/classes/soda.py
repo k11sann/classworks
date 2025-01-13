5f4c5bc453cd9fb45d1613c79b5ebbef21a0e71d
kusann <kusann_student@mail.ru>
2025-01-13 17:39:41 +0500

    Update soda.py

--- Python/classes/soda.py
+++ Python/classes/soda.py
@@ -1,22 +1,18 @@
 class Soda:
     def __init__(self):
         self.type = 0
+        self.dict_type = {
+            1:'ВИШНЁВАЯ',
+            2:'ЛИМОННАЯ',
+            3:'КОКИ КОЛА'}
         
     def addType(self, number):
         self.type=number
           
     def show_my_drink(self):
-        match self.type:
-            case 1:
-                return "Газировка ВИШНЁВАЯ"
-            case 2:
-                return "Газировка ЛИМОННАЯ"
-            case 3:
-                return "Газировка КОКИ-КОЛА"
-            case _:
-                return "ОБЫЧНАЯ ГАЗИРОВКА"
+        return self.dict_type.pop(self.type)
         
-            
+
 soda=Soda()
 soda.addType(2)
 print(soda.show_my_drink())
