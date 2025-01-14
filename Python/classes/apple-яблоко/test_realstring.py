ab4f563e2a1b96b9380379c6e99167aa034b587f
kusann <kusann_student@mail.ru>
2025-01-14 13:16:22 +0500

    Create test_realstring.py

---
+++ Python/classes/apple-яблоко/test_realstring.py
@@ -0,0 +1,22 @@
+# Запуск тестов: python test_calculator.py -v
+
+# .assertEqual(a, b)	    a == b
+# .assertTrue(x)	        bool(x) is True
+# .assertFalse(x)	        bool(x) is False
+# .assertIs(a, b)	        a is b
+# .assertIsNone(x)	        x is None
+# .assertIn(a, b)	        a in b
+# .assertIsInstance(a, b)	isinstance(a, b)
+
+import unittest
+from RealString import rs
+        
+class Test(unittest.TestCase):
+    def test_odd(self):
+        self.assertEqual(rs.odd(), 1, "ne rabotaet")
+        
+    def test_odd_char(self):
+        self.assertEqual(rs.odd_char(), 5983, "ne rabotaet")
+
+if __name__ == '__main__':
+    unittest.main()
